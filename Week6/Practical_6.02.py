ee2d0635dc4d6a2b393b8ac558c749c6a68e74b8
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-31 12:30:40 +0100

    Practical_6.02.py

--- Week6/Practical_6.02.py
+++ Week6/Practical_6.02.py
@@ -11,7 +11,12 @@ squares = [4, 9, 16, 25, 36]
 squares.append(49)
 squares.append(64)
 squares.append(81)
+print(squares)
 
 for i in squares:
     result = math.sqrt(i)
     print(result, end="  ")
+
+# Output:
+# [4, 9, 16, 25, 36, 49, 64, 81]
+# 2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0
