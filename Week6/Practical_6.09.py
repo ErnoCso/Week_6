c764f5524b2256bbf2ee26bb4b001edefc106234
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-31 12:42:18 +0100

    Practical_6.09.py

--- Week6/Practical_6.09.py
+++ Week6/Practical_6.09.py
@@ -10,3 +10,7 @@ print(f"                  The original list: {squares}")
 
 squares.pop(0)
 print(f"The list is after the pop() command:    {squares}")
+
+# Output:
+#                   The original list: [4, 9, 16, 25, 36, 49, 64, 81, 100]
+# The list is after the pop() command:    [9, 16, 25, 36, 49, 64, 81, 100]
