5959b27dc3140e52a5d058c2390eaf20ce2865a3
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-31 13:40:18 +0000

    Portfolio_6.01.py

--- Week6/Portfolio_6/Portfolio_6.01.py
+++ Week6/Portfolio_6/Portfolio_6.01.py
@@ -4,15 +4,37 @@
 # representation of that number in binary (base 2).
 # Hint: This is in many ways a trick question. Think!
 
-def to_binary(num):
-    if num < 0:
-        return
-    return bin(num) [2:]
+def main():
+    def choice():
+        decision = input("Would you like to run again? ")
+        ans1 = ["Yes", "yes", "YES", "y", "Y"]
+        if decision in ans1:
+            main()
+        else:
+            exit()
 
-number = 12
-binary_representation = to_binary(number)
-print(binary_representation)
+    parameters = int(input("Please type in an integer: "))
 
+    def to_binary(num):
+        if num < 0:
+            return
+        return bin(num)[2:]
+
+    binary_representation = to_binary(parameters)
+    print(binary_representation)
+
+    choice()
+
+
+main()
 
 # Output:
-# 1100
+
+# Please type in an integer: 45
+# 101101
+# Would you like to run again? y
+# Please type in an integer: 67
+# 1000011
+# Would you like to run again? n
+# 
+# Process finished with exit code 0
