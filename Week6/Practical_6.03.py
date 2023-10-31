9b1262f59245aabcc306e927a568097a190080cb
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-31 12:34:05 +0100

    Practical_6.03.py

--- Week6/Practical_6.03.py
+++ Week6/Practical_6.03.py
@@ -8,7 +8,19 @@ import math
 squares = [4, 9, 16, 25, 36]
 
 squares.extend([121, 144, 169])
+print(squares)
 
 for i in squares:
     result = math.sqrt(i)
     print(f"The square root of {i} is {result}")
+
+# Output:
+# [4, 9, 16, 25, 36, 121, 144, 169]
+# The square root of 4 is 2.0
+# The square root of 9 is 3.0
+# The square root of 16 is 4.0
+# The square root of 25 is 5.0
+# The square root of 36 is 6.0
+# The square root of 121 is 11.0
+# The square root of 144 is 12.0
+# The square root of 169 is 13.0
